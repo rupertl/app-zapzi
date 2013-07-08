8023f2129dbcac5b559a5d94dbfd09658ac0ad08
Rupert Lane <rupert@rupert-lane.org>
2013-07-08 22:36:31 +0800

    Set error attribute correctly

--- lib/App/Zapzi/FetchArticle.pm
+++ lib/App/Zapzi/FetchArticle.pm
@@ -111,18 +111,19 @@ sub _fetch_url
 
     if (! $response->{success} || ! length($response->{content}))
     {
-        $self->_set_error("Failed to fetch $url: ");
+        my $error = "Failed to fetch $url: ";
         if ($response->{status} == 599)
         {
             # Internal exception to HTTP::Tiny
-            $self->error .= $response->{content};
+            $error .= $response->{content};
         }
         else
         {
             # Error details from remote server
-            $self->error .= $response->{status} . " ";
-            $self->error .= $response->{reason};
+            $error .= $response->{status} . " ";
+            $error .= $response->{reason};
         }
+        $self->_set_error($error);
         return;
     }
 
